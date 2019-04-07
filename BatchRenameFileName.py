c95bebf401eae2385f4ddc3b6b360c6893413f5f
523041285@qq.com <523041285@qq.com>
2019-04-07 23:58:56 +0800

    批量重命名文件

---
+++ BatchRenameFileName.py
@@ -0,0 +1,19 @@
+import os
+
+def rename():
+    i = 134
+    path = r"E:\SVM\data\benign"
+
+    filelist = os.listdir(path)   #该文件夹下所有的文件（包括文件夹）
+    for files in filelist:   #遍历所有文件
+        i = i + 1
+        Olddir = os.path.join(path, files)    #原来的文件路径
+        if os.path.isdir(Olddir):       #如果是文件夹则跳过
+                continue
+        filetype = '.bmp'
+        Newdir = os.path.join(path, str(i) + filetype)   #新的文件路径
+        os.rename(Olddir, Newdir)    #重命名
+    return True
+
+if __name__ == '__main__':
+    rename()
