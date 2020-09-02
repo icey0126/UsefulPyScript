5ebd0f69562335af242757039743026248ec0e43
icey0126 <523041285@qq.com>
2020-09-02 21:29:55 +0800

    post

--- post_base64.py
+++ post_base64.py
@@ -1,9 +1,9 @@
 import requests
 import base64
 
-url = requests.post('http://xxx.com/')
+url = requests.post('http://test.com/')
 key = url.headers['flag']
 flag = base64.b64decode(key).decode().split(':')[1]
 re = {'key':flag}
-f = requests.post('http://xxx.com/',data = re)
+f = requests.post('http://test.com/',data = re)
 print(f.text)
